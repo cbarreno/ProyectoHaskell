82845ebdf2d690040b4db42906e702c19015217e
JordyVasquez <jgvasque@espol.edu.ec>
2014-01-19 02:10:58 -0500

    Funciones documentadas de mi parte del proyecto

--- proyectoHaskell.hs
+++ proyectoHaskell.hs
@@ -1,120 +1,30 @@
 {-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
--- import Text.XML.HXT.Core
+
 import System.IO
 import Data.List
 
--- Fuente: http://www.haskell.org/haskellwiki/HXT/Practical/Simple2 
--- This example demonstrates a more complex XML parse,
--- involving multiple levels, attributes, inner lists,
--- and dealing with optional data.
  
--- Example data drawn from:
--- http://www.ibiblio.org/xml/books/bible/examples/05/5-1.xml
--- save as: simple2.xml
- 
-data Device = Device 
-  { idD, userAgent, fallBack :: String,
-    groups :: [Group] }
-  deriving (Show, Eq)
- 
-
-data Group = Group
-  { idG :: String,
-	capabilitys :: [Capability]}
-	deriving (Show, Eq)
-
-data Capability = Capability
-    {name,value :: String }
-  deriving (Show, Eq)
- 
--- parseXML file = readDocument [ withValidate no
-                             -- , withRemoveWS yes  -- throw away formating WS
-                             -- ] file
- 
--- atTag tag = deep (isElem >>> hasName tag)
-  
-  
- 
--- Incremental development of the getTeams function:
- 
--- Try capturing some statistics about the players
- 
--- significant = not . all (`elem` " \n\r\t")
- 
--- Use our definition of "significant" strings to
--- capture the value; or else nothing.
- 
--- getStat attr = 
-  -- (getAttrValue attr >>> isA significant >>> arr Just)
-    -- `orElse` (constA Nothing)
-
--- getCapabilities = atTag "capability" >>>
-  -- proc c -> do
-    -- name    <- getAttrValue "name" -< c
-    -- value    <- getAttrValue "value"    -< c
-    -- returnA -< Capability
-      -- { name = name,
-        -- value = value}
-
--- getGroups = atTag "group" >>>
-  -- proc p -> do
-    -- id    <- getAttrValue "id" -< p
-    -- capabilitys  <- listA getCapabilities  -< p
-    -- returnA -< Group
-      -- { idG = id,
-        -- capabilitys  = capabilitys}
-	 
--- getDevices = atTag "device" >>>
-  -- proc l -> do
-    -- id <- getAttrValue "id" -< l
-    -- userAgent <- getAttrValue "user_agent" -< l
-    -- fallBack <- getAttrValue "fall_back" -< l
-    -- groups  <- listA getGroups    -< l
-    -- returnA -< Device
-      -- { idD   = id,
-        -- userAgent = userAgent,
-        -- fallBack = fallBack,
-        -- groups     = groups}
- 
--- Our final choices
-
-
---MyShow s =  concat  [ "[" , intersperse ',' s ,  "]" ]
+--Se define una estructura llamada Arbol , la cual puede ser una Hoja o un Nodo con una lista como contenido y una lista de Arboles.
 data Arbol a = Hoja | Nodo [a] [Arbol a ]	
 			   deriving (Show, Eq)
 			   
--- data Etiqueta a = Nombre 
-			   -- deriving (Show, Eq)
-			   
-						
--- data Device = Device 
-  -- { idD, userAgent, fallBack :: String,
-    -- groups :: [Group] }
-  -- deriving (Show, Eq)
- 			   
-			   			   
--- listaAr::String -> {Arbol a}
--- listaAr = [Arbol a]
-
-		
--- a1 = Nodo 10 [a11,a13]
-	-- where
-	-- a11 = Nodo [22,9] [Hoja,Hoja]
-	-- a12 = Nodo [35] [Hoja]
-	-- a13 = Nodo [52] [Hoja]
-
--- 
-	 
-crearArbol :: [a] -> Arbol a
-crearArbol []=Hoja
-crearArbol (x:xs) 
-	| length (x:xs)==1 = Nodo [x] [Hoja]
-	|otherwise = Nodo [x] [crearArbol (xs)]
-	-- pertenece :: Eq a => a -> Arbol a -> Bool
--- pertenece _ Hoja = False
--- pertenece x (Nodo r xs) = x == r || or (map (pertenece x ) xs)
---tipoEtiqueta :: [a] -> a
-								 
+--Esta funcion recibe :
+--Una lista y un arbol e inserta en el primer elemento 
+--de la lista del arbol que se recibe ,un arbol con el contenido de la lista recibida.
+insertarArbol e Hoja = Nodo e [Hoja]
+insertarArbol e (Nodo x xs)
+  | xs == [Hoja] = Nodo x (tail (xs ++ [Nodo e [Hoja]]))
+  | xs==xs = Nodo x (xs ++ [Nodo e [Hoja]])
+ 
+ --Esta funcion recibe :
+--Dos Arboles ,e inserta el primer arbol como elemento de la lista de arboles del segundo Arbol
+insertarNodoenArbol Hoja (Nodo x xs) = (Nodo x xs)
+insertarNodoenArbol nodo (Nodo x xs)
+  | xs == [Hoja] = Nodo x (tail (xs ++ [nodo]))
+  | xs==xs = Nodo x (xs ++ [nodo])
+  
+--Esta funcion resive una lista con una etiqueta y comprueba 
+--si es una etiqueta cerrada, el principio de una cadena abierta o el fin de esta 		 
 compruebaEtiqueta0 []= 0					
 compruebaEtiqueta0 (x:xs) 
 	| head (x:xs) == "<" && removeNon (head (tail (x:xs))) == ["1"] && last (x:xs) == ">" && last (init (x:xs))=="/" 	=  1
@@ -122,21 +32,15 @@ compruebaEtiqueta0 (x:xs)
 	| head (x:xs) == "<" && removeNon (head (tail (x:xs))) == ["1"] &&  last (x:xs) == ">" && removeNon (last (init (x:xs))) == ["1"] = 2
 	| head (x:xs) == "<" && head (tail (x:xs)) == "/" &&  last (x:xs) == ">" && removeNon (last (init (x:xs))) == ["1"]  = 3
 	|otherwise = 4
-	--insertarArbol :: Ord a => a -> Arbol a -> Arbol a
-insertarArbol e Hoja = Nodo e [Hoja]
-insertarArbol e (Nodo x xs)
-  | xs == [Hoja] = Nodo x (tail (xs ++ [Nodo e [Hoja]]))
-  | xs==xs = Nodo x (xs ++ [Nodo e [Hoja]])
- 
-insertarNodoenArbol Hoja (Nodo x xs) = (Nodo x xs)
-insertarNodoenArbol nodo (Nodo x xs)
-  | xs == [Hoja] = Nodo x (tail (xs ++ [nodo]))
-  | xs==xs = Nodo x (xs ++ [nodo])
- 
+	
+--Recibe una lista de lineas y le quita el espacio a cada una 
 quitaEspacio x
 	| x==[] = []
 	| x==x =lines(unwords(words( unwords [head x]))) ++ quitaEspacio (tail x)
 	
+--Esta funcio recibe :
+--Una lista que en el programa es la lista con los caracteres , comprueba si el archivo esta bien escrito 
+--, realiza el parseo y guada los datos en un Arbol
 parse0 [] = (0,Hoja)
 parse0 (x:xs) =	do	
 					let var=compruebaEtiquetaVersion (words( myShow (head (x:xs))))
@@ -150,7 +54,8 @@ parse0 (x:xs) =	do
 												parse1 (xs) a []
 								2 -> (2,Hoja)
 								0 -> (0,Hoja)
-							
+				
+--Recibe una lista y devuelve si el contenido es un xml pero sin la parte de la vesion 			
 parse1 (x:xs) a listanombres= do
 									let lon=length (x:xs)
 									let var=compruebaEtiqueta0 (words( myShow (head (x:xs))))
@@ -229,7 +134,7 @@ parse1 (x:xs) a listanombres= do
 					
 
 
-					
+--Recibe una lista y devuelve el primer nombre que encuentre
 obtenerNombre []=([],"")								
 obtenerNombre (x:xs)
 	 | x=="<" && head(xs) == "?" = obtenerNombre (tail(xs))
@@ -241,66 +146,88 @@ obtenerNombre (x:xs)
 	 | x=="="= ((xs),"")
 	 | otherwise = (fst(obtenerNombre(xs)) ,x ++ snd (obtenerNombre(xs)))		
 
-obtenerDatos []=([],"")								
+	 
+--Recibe una lista y devuelve el primer nombre o string que encuentre que cumpla
+-- en ser el valor de un atributo						 
+obtenerDatos []=([],"")			
 obtenerDatos(x:xs)
 	 | x=="="&&head(xs)=="\""&& removeNon (head(tail(xs)))==["1"] = (fst (obtenerDatos(tail(tail(xs)))) ,head(tail (xs)) ++ snd(obtenerDatos(tail(tail(xs)))))
 	 | x=="="&&head(xs)=="\""&& head(tail(xs))=="\"" = (tail(tail(xs)),"")
 	 | x=="="&&head(xs)=="\""&& isNum (head(tail(xs)))==["1"] = (fst (obtenerDatos(tail(tail(xs)))) ,head(tail (xs)) ++ snd(obtenerDatos(tail(tail(xs)))))
 	 | x=="\"" =((xs),"")
 	 | otherwise = (fst(obtenerDatos(xs)) ,x ++ snd (obtenerDatos(xs)))	
+	 
+--Recibe una cadena y devuelve el nombre de un Capability 	 
 obtenerNombreC []=([],"")								
 obtenerNombreC (x:xs)
 	 | x=="n" && head(xs) == "a"&& head(tail(xs))=="m" && head(tail(tail(xs))) == "e" = obtenerDatos(tail(tail(tail(xs))))--obtenerNombre (tail(xs))
-	 | otherwise = ([],"error")		
+	 | otherwise = ([],"error")	
 	 
+--Recibe una cadena y devuelve el value de un Capability 	  
 obtenerValueC []=([],"")								
 obtenerValueC (x:xs)
 	 | x=="v" && head(xs) == "a"&& head(tail(xs))=="l" && head(tail(tail(xs))) == "u" && head(tail(tail(tail(xs))))=="e" = obtenerDatos(tail(tail(tail(tail(xs)))))--obtenerNombre (tail(xs))
    	 | otherwise = ([],"error")	
 	 
+--Recibe una cadena y devuelve el id de un Group	 
 obtenerIdG []=([],"")								
 obtenerIdG(x:xs)
 	 | x=="i" && head(xs) == "d"  = obtenerDatos(tail(xs))
    	 | otherwise = ([],"error")	
 	 
+--Recibe una cadena y devuelve el id de un Device 	 
 obtenerIdD []=([],"")								
 obtenerIdD(x:xs)
 	 | x=="i" && head(xs) == "d"  = obtenerDatos(tail(xs))
    	 | otherwise = ([],"error")		
-obtenerFallBackD []=([],"")								
+	 
+--Recibe una cadena y devuelve el FallBack de un Device	 
+obtenercD []=([],"")								
 obtenerFallBackD (x:xs)
 	 | x=="f" && head(xs) == "a"&& head(tail(xs))=="l" && head(tail(tail(xs))) == "l" && head(tail(tail(tail(xs))))=="_" && head(tail(tail(tail(tail(xs)))))=="b" && head(tail(tail(tail(tail(tail(xs))))))=="a" && head(tail(tail(tail(tail(tail(tail(xs)))))))=="c" && head(tail(tail(tail(tail(tail(tail(tail(xs))))))))=="k"  = obtenerDatos(tail(tail(tail(tail(tail(tail(tail(tail(xs)))))))))
 	 | otherwise = ([],"error")	
+	 
+--Recibe una cadena y devuelve el UserAgent de un Device	 	 
 obtenerUserAgentG []=([],"")								
 obtenerUserAgentG (x:xs)
 	| x=="u" && head(xs) == "s"&& head(tail(xs))=="e" && head(tail(tail(xs))) == "r" && head(tail(tail(tail(xs))))=="_" && head(tail(tail(tail(tail(xs)))))=="a" && head(tail(tail(tail(tail(tail(xs))))))=="g" && head(tail(tail(tail(tail(tail(tail(xs)))))))=="e" && head(tail(tail(tail(tail(tail(tail(tail(xs))))))))=="n" && head(tail(tail(tail(tail(tail(tail(tail(tail(xs)))))))))=="t"  = obtenerDatos(tail(tail(tail(tail(tail(tail(tail(tail(tail(xs))))))))))
 	| otherwise = ([],"error")
 				
+--Esta funcion recibe una lista y comprueba si comienza como una version de xml				
 compruebaEtiquetaVersion (x:xs)
 	| head (x:xs) == "<" && head (tail (x:xs)) == "?" && last (x:xs) == ">" && last (init (x:xs))=="?" = 1
 	|(x:xs)==[] = 0
 	|otherwise = 2
 	
+	
+--Resive una lista en la cual esta parte del xml y 
+--devuelve otra lista pero sin la etiqueta de evrsion de cierre
 devuelveSinVersion (x:xs)	
 	| snd(obtenerNombre (words( myShow (head(words(head (x:xs)))))))=="version" = (x:xs)
 	| otherwise = devuelveSinVersion (xs)	
+	
+--Esta funcion recibe un Char y devuelve si es letra minuscula	
 removeNon st = do
 				let ca=[ c | c <- st, c `elem` ['a'..'z']]		
 				if null ca
 					then return  "0"
 					else do return  "1"
-					
+	
+--Esta funcion recibe un Char y devuelve si es numero	
 isNum st = do
 				let ca=[ c | c <- st, c `elem` ['0'..'9']]		
 				if null ca
 					then return  "0"
 					else do return  "1"					
-hoja :: [a] -> Arbol a 
-hoja x = Nodo x []
 
+
+--Esta funcion recibe un String y retorna el mismo 
+--string pero con un salto de linea por cada letra 
 myShow :: String -> String
 myShow s = concat [ intersperse '\n' s]
 
+--Esta funcion recibe el archivo xml y retora 1 si esta correcto, 
+--2 si esta incorecto y cero si el archivo estaba vacio
 pparsexml e= do
 				contents <- readFile e 
 				let ao = quitaEspacio(lines contents)
@@ -312,6 +239,8 @@ pparsexml e= do
 									1 -> return x--"El archivo es correcto"
 									2 -> return x--"El archivo es incorrecto"
 									0 -> return x--"no hay archivo"
-					
+									
+									
+--El main que llama a la funcion de parseo con el archivo a revisar					
 main = do pparsexml "martes.xml"
 
